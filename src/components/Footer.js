7a1461e356e37843c78b9fc9b020baaffc878f86
MattBarnabo <mattbarnabo@gmail.com>
2024-07-17 11:43:26 -0400

    adds Footer text

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,11 @@
-import React from "react"
+import React from "react";
 
 const Footer = () => {
-	return <div>Footer</div>
-}
+	return (
+		<div className="fixed right-6 bottom-0 ">
+			<p className="">&copy; Morgan | Matt | Book Wormz 2024</p>
+		</div>
+	);
+};
 
-export default Footer
+export default Footer;
