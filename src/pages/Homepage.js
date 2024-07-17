7fd76deb806d94e8b1df550118e88ebef50ec138
MattBarnabo <mattbarnabo@gmail.com>
2024-07-17 17:14:31 -0400

    adds text and styling for categories

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -4,7 +4,8 @@ import Card from "../components/Card";
 const Home = () => {
 	return (
 		<div className="w-screen h-screen flex justify-center">
-			<div className="w-[80%] h-[300px] flex justify-around">
+			<div className="w-[80%] h-[300px] flex justify-around flex-col">
+				<h2>Newest Fiction</h2>
 				{/* <Card /> */}
 			</div>
 		</div>
