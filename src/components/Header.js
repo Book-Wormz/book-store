c44226f3aeff9f67a66dfec581cead356da250e3
MattBarnabo <mattbarnabo@gmail.com>
2024-07-17 11:48:27 -0400

    adds Logo, NavBar, SearchBar, Shopping cart, and styling to Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,32 @@
 import React from "react";
 import NavBar from "./NavBar";
 import SearchBar from "./SearchBar";
+import { Link, NavLink } from "react-router-dom";
+import Logo from "../assets/Book-removebg-preview.png";
+import { AiOutlineShoppingCart } from "react-icons/ai";
+
+import "../App.css";
 
 const Header = () => {
 	return (
-		<div>
-			<NavBar />
-			<SearchBar />
+		<div className="back">
+			<div className="flex justify-between pl-5 pr-5">
+				<NavLink>
+					<img
+						className="h-60 w-60 mt-[-30px]"
+						src={Logo}
+						alt="Book worms logo"
+					/>
+					{/* <div className="flex justify-center items-center mt-[-55px] w-[50%]"></div> */}
+				</NavLink>
+				<div className="w-[90%] mt-[-55px] flex items-center justify-evenly ">
+					<NavBar />
+					<SearchBar />
+					<Link className="">
+						<AiOutlineShoppingCart className="h-8 w-8" />
+					</Link>
+				</div>
+			</div>
 		</div>
 	);
 };
