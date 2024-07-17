a71e6757655856a51a422896775d6dff9dd99af6
MattBarnabo <mattbarnabo@gmail.com>
2024-07-17 17:13:57 -0400

    adds styling for the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
 	return (
-		<div className="fixed right-6 bottom-0 ">
+		<div className="fixed right-6 bottom-2 ">
 			<p className="">&copy; Morgan | Matt | Book Wormz 2024</p>
 		</div>
 	);
