2c6e5cf173fe4a427083fc0b1d396c42fc7d140f
MattBarnabo <mattbarnabo@gmail.com>
2024-07-17 11:45:26 -0400

    adds NavButtons

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,22 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import NavButton from "./NavButton";
 
 const NavBar = () => {
 	return (
 		<div>
-			{/* image */}
-			<NavLink />
+			<NavButton url="/" buttonContent="Books" />
+			<span>|</span>
+			<NavButton url="/" buttonContent="Fiction" />
+			<span>|</span>
+			<NavButton url="/" buttonContent="Nonfiction" />
+			<span>|</span>
+			<NavButton url="/" buttonContent="Teen & YA" />
+			<span>|</span>
+			<NavButton url="/" buttonContent="Kids" />
+			<span>|</span>
+			<NavButton url="/" buttonContent="eBooks" />
+			<span>|</span>
+			<NavButton url="/" buttonContent="Magazines" />
 		</div>
 	);
 };
