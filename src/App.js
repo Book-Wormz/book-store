ba8b95f2a92861e7e7c87e6ae1e605314ac9ef20
Morgan <smorgannicole@gmail.com>
2024-07-18 13:53:59 -0500

    search results page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Homepage";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import SearchResults from "./pages/SearchResults";
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/search-results" element={<SearchResults />} />
       </Routes>
       <Footer />
     </>
---
+++ src/pages/SearchResults.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { useLocation } from "react-router-dom";
+
+const SearchResults = () => {
+  const location = useLocation();
+  const { books, error, query } = location.state;
+  // destructuring location.state- accesses state data passed previously in SearchBar
+
+  if (error) return <div>Error: {error}</div>;
+  if (books?.length === 0) return <div>No books found for: {query}</div>;
+
+  return (
+    <>
+      <h1>Search Results for: {query}</h1>
+      <div>
+        {books?.map((book) => (
+          <ul key={book.id}>
+            <li>{book.volumeInfo.title}</li>
+            <li>{book.volumeInfo.authors?.join(", ")}</li>
+            {/* .join(", ") method doesn't return an error if there's only one str in the arr- it just returns that single string */}
+          </ul>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SearchResults;
