f8d97bf59cd81992d8938091cc4af63c4636b6cc
MattBarnabo <mattbarnabo@gmail.com>
2024-07-22 14:19:23 -0400

    adds change for bookgrid props pipeline

--- src/components/BookGrid.js
+++ src/components/BookGrid.js
@@ -1,11 +1,11 @@
 import Card from "../components/Card";
 
-const BookGrid = () => {
+const BookGrid = ({ genre, volumes, sort }) => {
 	return (
 		<div className="w-screen h-screen flex flex-col items-start">
 			<div className="w-full">
 				<h2>Newest Fiction</h2>
-				<Card genre="fiction" volumes="&maxResults=40" sort="&orderBy=newest" />
+				<Card genre={genre} volumes={volumes} sort={sort} />
 			</div>
 		</div>
 	);
