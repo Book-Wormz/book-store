5cb4700a96ad558f213186e7945896684de5cf65
MattBarnabo <mattbarnabo@gmail.com>
2024-07-24 14:37:50 -0400

    adds different book grids

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -2,7 +2,41 @@ import React from "react";
 import BookGrid from "../components/BookGrid";
 
 const Home = () => {
-	return <BookGrid />;
+	return (
+		<div>
+			<h1>BOOKS</h1>
+			<h2>New in fiction</h2>
+			<BookGrid
+				genre="subject:fiction"
+				volumes="&maxResults=6"
+				sortOrder="&orderBy=newest"
+			/>
+			<h3>New in NonFiction</h3>
+			<BookGrid
+				genre="subject:nonfiction"
+				volumes="&maxResults=6"
+				sortOrder="&orderBy=newest"
+			/>
+			<h4>New in Classics</h4>
+			<BookGrid
+				genre="subject:classics"
+				volumes="&maxResults=6"
+				sortOrder="&orderBy=newest"
+			/>
+			<h5>New in History</h5>
+			<BookGrid
+				genre="subject:history"
+				volumes="&maxResults=6"
+				sortOrder="&orderBy=newest"
+			/>
+			<h6>New in Fantasy</h6>
+			<BookGrid
+				genre="subject:fantasy"
+				volumes="&maxResults=6"
+				sortOrder="&orderBy=newest"
+			/>
+		</div>
+	);
 };
 
 export default Home;
