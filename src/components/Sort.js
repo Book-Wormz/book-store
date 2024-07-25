27c86a3c9964dbebb12e9ef52242f79e3a63cc98
Morgan <smorgannicole@gmail.com>
2024-07-25 15:55:55 -0500

    current display order shown on btn

--- src/components/Sort.js
+++ src/components/Sort.js
@@ -2,29 +2,35 @@ import React, { useState } from "react";
 import BookGrid from "./BookGrid";
 
 const SortButton = ({ genre, volumes }) => {
-	const [sortOrder, setSortOrder] = useState("");
-	const handleSort = (order) => {
-		setSortOrder(order);
-	};
+  const [sortOrder, setSortOrder] = useState("");
+  const [displayedOrder, setDisplayedOrder] = useState("Relevance");
+  const handleSort = (order) => {
+    setSortOrder(order);
+    if (order.includes("relevance")) {
+      setDisplayedOrder("Relevance");
+    } else {
+      setDisplayedOrder("Newest");
+    }
+  };
 
-	return (
-		<>
-			<details className="dropdown">
-				<summary className="btn m-1">sort by</summary>
-				<ul className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
-					<li>
-						<div onClick={() => handleSort("&orderBy=relevance")}>
-							Relevance
-						</div>
-					</li>
-					<li>
-						<div onClick={() => handleSort("&orderBy=newest")}>Newest</div>
-					</li>
-				</ul>
-			</details>
-			<BookGrid genre={genre} volumes={volumes} sortOrder={sortOrder} />
-		</>
-	);
+  return (
+    <>
+      <details className="dropdown p-10">
+        <summary className="btn m-1">Sort by: {displayedOrder}</summary>
+        <ul className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
+          <li>
+            <div onClick={() => handleSort("&orderBy=relevance")}>
+              Relevance
+            </div>
+          </li>
+          <li>
+            <div onClick={() => handleSort("&orderBy=newest")}>Newest</div>
+          </li>
+        </ul>
+      </details>
+      <BookGrid genre={genre} volumes={volumes} sortOrder={sortOrder} />
+    </>
+  );
 };
 
 export default SortButton;
